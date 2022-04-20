665775c9f1ebb3c56fa976b886deb7216aa44e72
Matt <matthewbertweb@gmail.com>
2022-04-20 13:08:23 -0400

    I added my first array to display in an alert box

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Name Alert - Assigning Value to Variables
 let firstName = 'Maddie';
 let lastName = 'Jordan';
+// Food Alert
+let favoriteFood = ['BBQ', 'Pizza', 'Chinese'];
 
-
+// Name Alert - using a string to display our variables in a sentance
 alert('Hello, my name is ' + firstName + ' and my last name is ' + lastName);
+
+// Food Alert - displaying items from an array
+alert(favoriteFood);
