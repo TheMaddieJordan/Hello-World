0c009abd35ed8423785cba733f45fa7215b3ccdc
Matt <matthewbertweb@gmail.com>
2022-04-20 16:32:41 -0400

    I added proper notes in the bio.js file

--- bio.js
+++ bio.js
@@ -2,11 +2,12 @@ let firstName = 'Maddie';
 let lastName = 'Jordan';
 let fullName = firstName + ' ' + lastName;
 let birthYear = 1986;
+// new Date() is a method
 let year = new Date();
+// getFullYear() will get the full current year
 let age = year.getFullYear() - birthYear;
-
-//console.log(y.getFullYear() - birthYear);
 
 //document.getElementById('biography').innerHTML = y.getFullYear() - birthYear;
 
+//document.getElementById will help us grab an element from HTML
 document.getElementById('biography').innerHTML = 'Hello World, my name is ' + fullName + ' and I am ' + age;
