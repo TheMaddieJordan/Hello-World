b2db491e69de706cc4a1d05b6fa2c016c04ada91
Matt <matthewbertweb@gmail.com>
2022-05-16 16:48:18 -0400

    added the random facts sections to the html and js files.

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 I have been out of the JS and code game for over a year, as my focus with work for the time was WordPress design. This little "Hello World" project was just to refresh my basics of setting up files, and refreshing my memory on how to use Git properly.
 
-## 4/40/2022
+## 4/20/2022
 I learned more about variables, strings, and numbers today. I messed around with console.log() for a little while, but I then added a getElementById to display a string. I wanted to display my age by subtracting my birth year from the current year, so I learned about the getFullYear() method.
+
+## 5/16/2022
+I learned a little bit about functions, arrays, Math.floor(), Math.random(), and EventListeners. I used what I learned to add a random fact generator to the page.
--- bio.js
+++ bio.js
@@ -7,7 +7,38 @@ let year = new Date();
 // getFullYear() will get the full current year
 let age = year.getFullYear() - birthYear;
 
+
 //document.getElementById('biography').innerHTML = y.getFullYear() - birthYear;
 
 //document.getElementById will help us grab an element from HTML
 document.getElementById('biography').innerHTML = 'Hello World, my name is ' + fullName + ' and I am ' + age;
+
+
+// Random Facts
+const facts = [
+    'I am obsessed with Batman',
+    'I am a transwoman',
+    'I love yoga & weightlifting',
+    'I am originally from St. Louis, and I now live in Dunedin FL'
+];
+// Target the button
+const btn = document.getElementById('btn');
+// Color value
+const fact = document.querySelector('.fact');
+
+btn.addEventListener('click', function() {
+    // get a random number between 0 - 3
+   const randomNumber = getRandomNumber();
+   console.log(randomNumber);
+   // get into the array
+   document.body.style.backgroundColor = facts[randomNumber];
+   // select the color
+   fact.textContent = facts[randomNumber];
+});
+
+// Generate random number
+function getRandomNumber() {
+    // Math.floor() will round down to the nearest full number
+    // Math.random() will help us select a random number from the array
+    return Math.floor(Math.random() * facts.length);
+}
